470e5cb4b0aa87763f7da8f03fcf7b15bbb0300a
grace.christina <grace.christina@go-jek.com>
2018-10-06 22:48:39 +0800

    Added the folder structure

--- cmd/kacung-bus/main.go
+++ cmd/kacung-bus/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/nlopes/slack"
 )
 
 func main() {
-	fmt.Println("test")
+	api := slack.New("2JxJ0UclRDLYhslNxX1Rk7Xp")
+	user, err := api.GetUserInfo("450073692785.452043493622")
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+	fmt.Printf("ID: %s, Fullname: %s, Email: %s\n", user.ID, user.Profile.RealName, user.Profile.Email)
 }
